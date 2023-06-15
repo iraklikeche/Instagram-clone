2e67685bb782ee6be67262c8b27672e5137f4b08
iraklikeche <irakli.kechee@gmail.com>
2023-06-15 15:11:17 +0400

    uploade photo

--- src/components/UploadPhotoModal.vue
+++ src/components/UploadPhotoModal.vue
@@ -1,26 +1,48 @@
 <script setup>
 import { ref } from "vue";
+import { supabase } from "../supabase";
 
 const visible = ref(false);
+const caption = ref("");
+const file = ref(null);
 
 const showModal = () => {
   visible.value = true;
 };
 
-const handleOk = (e) => {
-  console.log(e);
-  visible.value = false;
+const handleOk = async () => {
+  const fileName = Math.floor(Math.random() * 1000000000000000);
+  if (file.value) {
+    const response = await supabase.storage
+      .from("images")
+      .upload("public/" + fileName, file.value);
+    console.log({ response });
+  }
 };
 
+const handleUploadChange = (e) => {
+  if (e.target.files[0]) {
+    file.value = e.target.files[0];
+  }
+};
 </script>
 
 <template>
   <div>
-    <a-button  @click="showModal">Upload Photo</a-button>
+    <a-button @click="showModal">Upload Photo</a-button>
     <a-modal v-model:visible="visible" title="Basic Modal" @ok="handleOk">
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
+      <input type="file" accept=".jpeg,.png" @change="handleUploadChange" />
+      <a-input
+        v-model:value="caption"
+        :maxLength="50"
+        placeholder="Caption..."
+      />
     </a-modal>
   </div>
 </template>
+
+<style scoped>
+input {
+  margin-top: 10px;
+}
+</style>
